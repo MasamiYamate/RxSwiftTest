f9feca9b9c22a67d0b6923338248e9bf074c0594
MasamiYamate <m.yamate@mail.yamatte.net>
2019-05-08 09:12:49 +0900

    init

---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by MasamiYamate on 2019/05/08.
+//  Copyright © 2019 MasamiYamate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
